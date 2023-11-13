889e2aeae685f81dd84b99757e00d3121821dc74
LucasLaho <lucas.lahougue@etudiant.univ-rennes1.fr>
2023-11-13 10:46:35 -0500

    added connection page

--- app/src/main/java/com/example/piclyt/MainActivity.kt
+++ app/src/main/java/com/example/piclyt/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.piclyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.piclyt.ui.theme.PicLytTheme
@@ -17,22 +26,77 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PicLytTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("PicLyt")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
+/* Fonctionnement global de l'application */
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    Surface(modifier, color = MaterialTheme.colorScheme.background) {
+        Connection()
+    }
+}
+
+/* Preview de l'application */
+@Preview
+@Composable
+fun MyAppPreview() {
+    PicLytTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+/* Ecran de connexion */
+@Composable
+fun Connection(modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        var usernameText by remember { mutableStateOf("") }
+        var passwordText by remember { mutableStateOf("") }
+        SimpleOutlinedTextFieldSample("Nom d'utilisateur", onValueChange = { usernameText = it })
+        SimpleOutlinedTextFieldSample("Mot de passe", onValueChange = { passwordText = it })
+    }
+}
+
+/* Preview de l'ecran de connexion */
+@Preview(showBackground = true)
+@Composable
+fun ConnectionPreview() {
+    PicLytTheme {
+        Connection()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SimpleOutlinedTextFieldSample(hint: String, onValueChange: (String) -> Unit) {
+    var text by remember { mutableStateOf("") }
+    OutlinedTextField(
+        value = text,
+        onValueChange = {
+            text = it
+            onValueChange(it)
+        },
+        placeholder = { Text(hint) },
+        singleLine = true
+        )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SimpleOutlinedTextFieldSamplePreview() {
+    PicLytTheme {
+        SimpleOutlinedTextFieldSample("blblblb", onValueChange = {})
+    }
+}
+
+/*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello World, master $name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
@@ -41,6 +105,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PicLytTheme {
-        Greeting("PicLyt")
+        Greeting("Android")
     }
 }
+*/
